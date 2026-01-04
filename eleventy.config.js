3ebadcba79ee1cb227450130cef6353381301d25
Anthony Gardes <aka.antoes@gmail.com>
2026-01-04 15:06:09 -0800

    added passthorugh file copy

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,3 +3,18 @@ const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 };
+
+module.exports = function (eleventyConfig) {
+	// Output directory: _site
+
+	// Copy `img/` to `_site/img/`
+	eleventyConfig.addPassthroughCopy("img");
+
+	// Copy `css/fonts/` to `_site/css/fonts/`
+	// Keeps the same directory structure.
+	eleventyConfig.addPassthroughCopy("css/fonts");
+
+	// Copy any .jpg file to `_site`, via Glob pattern
+	// Keeps the same directory structure.
+	eleventyConfig.addPassthroughCopy("**/*.jpg");
+};
